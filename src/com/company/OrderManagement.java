21ff1f586b7d9a83205481d181b7fec6c19305a0
HugoCosta <huca@ipvc.pt>
2021-05-02 18:52:30 +0100

    new

---
+++ src/com/company/OrderManagement.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class OrderManagement extends JDialog {
+    private JPanel contentPane;
+    private JButton cancelOrderButton;
+    private JButton button1;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+
+    public OrderManagement() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        OrderManagement dialog = new OrderManagement();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
